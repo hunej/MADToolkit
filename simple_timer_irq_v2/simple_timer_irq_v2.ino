b36fee4f838c8581a5a92f0fc7e3eafc9be6149e
hunej <hunejaap@gmail.com>
2022-08-06 18:59:38 +0800

    Create simple_timer_irq_v2.ino

---
+++ simple_timer_irq_v2/simple_timer_irq_v2.ino
@@ -0,0 +1,179 @@
+#include <TimerOne.h>
+#include "TM1637.h"
+
+
+
+#define BUZZER_PIN 13
+#define BUTTON_START_PIN 8
+#define BUTTON_ADD_PIN 7
+#define BUTTON_SUB_PIN 6
+
+#define ISR_PERIOD 10//ms
+
+#define ON 1
+#define OFF 0
+
+#define CLK 2 // TM1637 CLK
+#define DIO 3 // TM1637 DIO
+
+TM1637 tm1637(CLK, DIO);
+
+int8_t TimeDisp[] = {0x00, 0x00, 0x00, 0x00};
+unsigned char ClockPoint = 1;
+unsigned char Update;
+unsigned long timer_10ms;
+unsigned long timer_10ms_init;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello");
+  pinMode(BUTTON_ADD_PIN, INPUT);
+  pinMode(BUTTON_SUB_PIN, INPUT);
+  pinMode(BUTTON_START_PIN, INPUT);
+  
+
+
+ // Buzzer
+  pinMode(BUZZER_PIN, OUTPUT); // BUZZER
+  digitalWrite(BUZZER_PIN, LOW);
+  beep_short(3);
+
+
+
+  // TM1637
+  tm1637.set();
+  tm1637.init();
+  tm1637.point(POINT_ON);
+  tm1637.display(TimeDisp);
+
+
+
+  timer_10ms = 0;
+  while (digitalRead(BUTTON_START_PIN) != LOW)
+  {
+    if (digitalRead(BUTTON_ADD_PIN) == LOW)
+    {
+      Serial.println("add pressed");
+      
+      timer_10ms += 100;
+      TimeUpdate(timer_10ms);
+      tm1637.display(TimeDisp);
+      delay(100);
+    }
+    else if (digitalRead(BUTTON_SUB_PIN) == LOW)
+    {
+      Serial.println("sub pressed");
+      if(timer_10ms>=100)
+      {
+      timer_10ms -= 100;
+      TimeUpdate(timer_10ms);
+      tm1637.display(TimeDisp);
+      delay(100);
+      }
+    }
+
+    delay(100);
+  }
+
+  Serial.println("Start!");
+  timer_10ms_init = timer_10ms;
+delay(10);
+  Timer1.initialize(10000);//10ms isr
+  Timer1.attachInterrupt(TimingISR);
+  
+}
+
+//void loop()
+//{
+//  Serial.println("haha haha");
+//
+//  delay(100);
+//}
+void(* resetFunc) (void) = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Update == ON)
+  {
+    TimeUpdate(timer_10ms);
+    tm1637.display(TimeDisp);
+//    Serial.println(digitalRead(BUTTON_ADD_PIN));
+  }
+//  Serial.println(digitalRead(BUTTON_ADD_PIN));
+  delay(10);
+
+
+  if(timer_10ms<=0)
+   {
+    beep_short(5);
+//    while(1);
+    while(digitalRead(BUTTON_START_PIN) != LOW);
+    Serial.println("Start!");
+    timer_10ms = timer_10ms_init;
+    TimeUpdate(timer_10ms);
+    tm1637.display(TimeDisp);    
+//    return;
+   }
+}
+
+
+void TimingISR()
+{ //10ms
+  
+  if(timer_10ms>0)
+  timer_10ms--;
+
+
+
+
+  
+//  if(timer_10ms%10==0)
+    Update = ON;
+  
+
+}
+
+int TimeUpdate(int time_input)
+{
+  int second, msecond;
+  if (ClockPoint)
+  {
+    tm1637.point(POINT_ON);
+  }
+  else
+  {
+    tm1637.point(POINT_OFF);
+  }
+
+  second = time_input / 100;
+  msecond = time_input % 100;
+
+  TimeDisp[0] = second / 10;
+  TimeDisp[1] = second % 10;
+  TimeDisp[2] = msecond / 10;
+  TimeDisp[3] = msecond % 10;
+  Update = OFF;
+
+  return 0;
+}
+
+void beep_short(int count)
+{
+  while (count--)
+  {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(100);
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(100);
+  }
+  digitalWrite(9, LOW);
+}
+void beep_long(int duration)
+{
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(duration * 1000);
+  digitalWrite(BUZZER_PIN, LOW);
+
+  delay(100);
+}
